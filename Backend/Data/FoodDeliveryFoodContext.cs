5969212121045c58963608ad54bb5a8c5464a1c5
Anand Raj <76206489+rajanandraj04@users.noreply.github.com>
2025-06-26 12:30:48 +0530

    Update FoodDeliveryFoodContext.cs

--- Backend/Data/FoodDeliveryFoodContext.cs
+++ Backend/Data/FoodDeliveryFoodContext.cs
@@ -31,8 +31,8 @@ public partial class FoodDeliveryFoodContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:dotnettrainingfood.database.windows.net,1433;Initial Catalog=Food_DeliveryFood;Persist Security Info=False;User ID=dotnetfood;Password=Ltimindtree!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        => optionsBuilder.UseSqlServer("Server=tcp:dotnettrainingfood.database.windows.net Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
